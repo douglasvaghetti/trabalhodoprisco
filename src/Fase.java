60d87f5a1096d0c9f2a3d474709994603a53982e
Hallan Neves <hallanneves@gmail.com>
2015-08-28 09:03:33 -0300

    Começando a interface e o objeto fase.

--- src/Fase.java
+++ src/Fase.java
@@ -1,16 +1,6 @@
 
 import java.util.ArrayList;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Hallan
- */
 public class Fase {
     
     private Macaco macaco;
@@ -24,6 +14,12 @@ public class Fase {
         }
     }
     
+    public void adicionaObjeto(Objeto obj){
+        this.atuadores.add(obj);
+    }
     
+    public void RemoveObjeto(Objeto obj){
+        this.atuadores.remove(obj);
+    }
     
 }
