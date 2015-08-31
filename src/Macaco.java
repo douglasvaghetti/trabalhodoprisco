8026149ebeedaca588cd2a399b6461b9077b0f6c
Hallan Neves <hallanneves@gmail.com>
2015-08-30 21:28:11 -0300

    Altera icones e adiciona o códico de carregar a imagem do macao.

--- src/Macaco.java
+++ src/Macaco.java
@@ -1,15 +1,38 @@
 
 import java.awt.Graphics;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 public class Macaco extends Objeto{
 
+    private Image imgMacaco;
+
+    
     public Macaco(int x, int y) {
         super(x, y);
+        this.setAltura(50);
+        this.setLargura(50);
+        URL url;
+        try {
+            url = new URL("file:///" + Ferramentas.getCodeBase() + "\\src\\imagens\\Macaco.png");
+            imgMacaco = ImageIO.read(url);
+        } catch (MalformedURLException ex) {
+            System.out.println("Erro ao formar a url da imagem.");
+            Logger.getLogger(Cartola.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            System.out.println("Erro ao carregar imagem.");
+            Logger.getLogger(Cartola.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void desenha(Graphics g) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
